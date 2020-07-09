7ca1dd77693e15cf5ae93667b92a12c15a539e2d
Evan Zhang <zhang21evan@ncssm.edu>
2020-07-09 13:14:09 -0400

    Commit

---
+++ failedscrape.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup as soup
+from urllib.request import Request, urlopen
+from datetime import date
+import math
+import openpyxl
+
+states_fullname = [
+'alabama', 'alaska', 'arkansas', 'arizona', 'california', 'colorado', 'connecticut', 'delware', 'florida', 'georgia', 'hawaii', 'idaho', 'illinois',
+'indiana', 'iowa', 'kansas', 'kentucky', 'louisiana', 'maine', 'maryland', 'massachusetts', 'michigan', 'minnesota', 'missouri', 'mississippi',
+'montana', 'nebraska', 'nevada', 'new-hampshire', 'new-jersey', 'new-mexico', 'new-york', 'north-carolina', 'north-dakota', 'ohio', 'oklahoma', 'oregon',
+'pennsylvania', 'rhode-island', 'south-carolina', 'south-dakota', 'tennessee', 'texas', 'utah', 'vermont', 'virginia', 'washington', 'west-virginia',
+'wisconsin', 'wyoming'
+]
+
+xfile = openpyxl.load_workbook('state_covid19.xlsx')
+sheet = xfile['Sheet1']
+diff = (date.today()-date(2020,7,7)).days + 168
+
+code = ""
+first_letter = math.floor((diff-1)/26)
+code += chr(ord('@')+first_letter)
+diff -= (first_letter*26)
+code += chr(ord('@')+diff)
+
+c = 2
+for states in states_fullname:
+  print(states)
+  fname = 'https://www.worldometers.info/coronavirus/usa/' + states + '/'
+  req = Request(fname, headers={'User-Agent': 'Mozilla/5.0'})
+  webpage = urlopen(req)
+  page_soup = soup(webpage, "html.parser")
+
+  containers = page_soup.findAll("table", {"id": "usa_table_countries_yesterday"})
+  container = containers[0].tbody.tr
+  temp_list = container.findAll("td")
+  num0 = str(temp_list[1])
+  num1 = num0.replace('<td>', '')
+  num = num1.replace('</td>', '')
+
+  sheet[str(code) + str(c)].value = num
+  c += 1
+
+xfile.save('state_covid19.xlsx')
