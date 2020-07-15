1996ec7a1bcd4989a4b1bf47fd43a7cab645539b
Evan Zhang <zhang21evan@ncssm.edu>
2020-07-15 11:56:18 -0400

    Commit

--- datasets.py
+++ datasets.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pytrends.request import TrendReq
 
-search_period = '2019-05-31 2020-05-31' #search the past year
+search_period = '2020-01-26 2020-07-05' #search the past year
 
 pytrends = TrendReq(hl='en-US', tz=420)
 kw_list = ["depression", "anxiety", "panic attack", "insomnia", "loneliness", "covid"] #list of keywords
@@ -9,8 +9,8 @@ kw_listextra = ["depression", "anxiety", "panic attack", "insomnia", "loneliness
 
 ### Building the dataframe
 df = pd.DataFrame({})
-for id in kw_list:
+for id in kw_listextra:
     pytrends.build_payload([id], geo='US', timeframe = search_period)
     df[id] = pytrends.interest_over_time()[id]
 
-df.to_excel('1_year_period.xlsx')
+df.to_excel('covid_period.xlsx')
