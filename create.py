23c45618e7b27a3b3a9876d9744003e7c5498e1f
Evan Zhang <zhang21evan@ncssm.edu>
2020-07-17 10:47:36 -0400

    Commit

--- create.py
+++ create.py
@@ -12,8 +12,8 @@ kw_list_help = ["rehab", "meditation", "online therapy", "herbs", "online counse
 
 ### Building the dataframe
 df = pd.DataFrame({})
-for id in kw_list_help:
-    pytrends.build_payload([id], geo='US', timeframe = search_period)
+for id in kw_list:
+    pytrends.build_payload([id], geo = 'US', timeframe = search_period)
     df[id] = pytrends.interest_over_time()[id]
 
-df.to_excel('treatment.xlsx')
+df.to_excel('fname')
